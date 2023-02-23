5f9b7f0a332a14eef3dd9402f9a61eb04f6dc7ab
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-23 23:54:11 +0530

    removed navigate hook

--- src/utils/signout.js
+++ src/utils/signout.js
@@ -2,8 +2,6 @@ import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 
 export default function signOutUser() {
-  // const navigate = useNavigate();
-
   signOut(auth)
     .then(() => {
       // Sign-out successful.
