d6ae020a7f339b34b1ea07338471853020240767
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-24 23:26:43 +0530

    rendering the selecting template component in homepage

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+import client from "../utils/client";
+
+import { Dropdown } from "../components";
 
 const CreateNote = () => {
+  const [templates, setTemplates] = useState([]);
+
+  useEffect(() => {
+    const fetchTemplates = async () => {
+      const resp = await client.fetch(`*[_type == "templates"]`);
+      setTemplates(resp);
+    };
+
+    fetchTemplates();
+  }, []);
+
   return (
-    <div className='mt-10 p-2'>
-        <div className='my-4'>
-            <h1 className='text-black text-3xl text-center'>Create a new note</h1>
+      <div className="mt-10 p-2">
+        <div className="my-4">
+          <h1 className="text-black text-3xl text-center">Create a new note</h1>
         </div>
 
-        <main className='my-2'>
-            
+        <main className="my-2 p-2 bg-gray-100">
+          <div className="text-black">
+            <h2 className="text-lg">Selected a note template</h2>
+
+            <Dropdown
+              title="Templates"
+              items={templates}
+              type="templates"
+            />
+          </div>
         </main>
+      </div>
+  );
+};
 
-
-    </div>
-  )
-}
-
-export default CreateNote
+export default CreateNote;
