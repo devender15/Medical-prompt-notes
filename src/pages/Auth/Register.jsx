381472289553a582972a58ac4db80e93c3f29379
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-24 23:20:38 +0530

    added disable button feature

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -86,16 +86,15 @@ const Register = () => {
                 <div className="bg-gray-50 px-4 py-3 text-right sm:px-6 flex space-x-2 items-center">
                   <button
                     type="submit"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:bg-gray-500"
+                    disabled={user?.email?.length === 0 || user?.password?.length === 0}
                   >
                     Register
                   </button>
 
-                  <button className="inline-flex justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
-                    <Link to="/login" className="text-white hover:text-white">
+                    <Link to="/login" className="inline-flex justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 hover:text-white">
                       Login
                     </Link>
-                  </button>
                 </div>
               </div>
             </form>
