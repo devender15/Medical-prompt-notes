97ed18a0ebfb4060bae4835f1deb7170a87e3bdf
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-24 23:03:06 +0530

    added templatedetails context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,17 @@ import { Routes, Route } from "react-router-dom";
 
 import { Notes, Note, CreateNote } from "./";
 
+import TemplateDetails from "../context/templateDetails";
+
 const Home = () => {
   return (
-    <Routes>
-      <Route path="/" element={<CreateNote />} />
-      <Route path="/view-notes" element={<Notes />} />
-      <Route path="/note/:slug" element={<Note />} />
-    </Routes>
+    <TemplateDetails>
+      <Routes>
+        <Route path="/" element={<CreateNote />} />
+        <Route path="/view-notes" element={<Notes />} />
+        <Route path="/note/:slug" element={<Note />} />
+      </Routes>
+    </TemplateDetails>
   );
 };
 
