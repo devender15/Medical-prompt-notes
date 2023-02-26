bc0446d1736e3724869a994471062c1b4ddb780d
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-26 20:01:45 +0530

    added card component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Navbar } from "../components/Navbar";
 export { default as Loader } from "../components/Loader";
+export { default as Card } from "../components/Card";
 export { default as Dropdown } from "./Dropdown";
 export { default as Modal } from "../components/Modal";
 export { default as FieldModal } from "../components/FieldModal";
