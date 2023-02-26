a9971f21b139a65b9d48e2a0ee1230a18e99ad39
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-26 20:01:56 +0530

    minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useContext } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 
-import { Notes, Note, CreateNote } from "./";
+import { Notes, EditNote, CreateNote } from "./";
 
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
@@ -25,8 +25,8 @@ const Home = () => {
   return (
     <Routes>
       <Route path="/" element={<CreateNote />} />
+      <Route path="/note/:slug" element={<EditNote />} />
       <Route path="/view-notes" element={<Notes />} />
-      <Route path="/note/:slug" element={<Note />} />
     </Routes>
   );
 };
