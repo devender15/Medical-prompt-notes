1fd7349c9032032180d85e308777eecfaa6707b6
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-26 19:21:15 +0530

    added new components

---
+++ src/components/FieldModal.jsx
@@ -0,0 +1,143 @@
+import React, { Fragment, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+
+import { FieldTypesDropdown } from "./";
+
+const FieldModal = ({ open, setOpen, setFields }) => {
+  const cancelButtonRef = useRef(null);
+  const [values, setValues] = useState({});
+
+  const handleInputChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const addFieldToArray = () => {
+    setFields((prev) => {
+      let arr = [...prev];
+      arr.push(values);
+      return arr;
+    });
+
+    setOpen(false);
+  };
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        initialFocus={cancelButtonRef}
+        onClose={setOpen}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-scroll rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <ExclamationTriangleIcon
+                        className="h-6 w-6 text-red-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-base font-semibold leading-6 text-gray-900"
+                      >
+                        New field
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <form className="flex flex-col space-y-2 my-3">
+                          <div>
+                            <label htmlFor="name" className="sr-only">
+                              Name
+                            </label>
+                            <input
+                              type="text"
+                              id="name"
+                              name="name"
+                              placeholder="Name"
+                              value={values["title"]}
+                              onChange={handleInputChange}
+                              required
+                              className="bg-white capitalize relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            />
+                          </div>
+
+                          <div>
+                            <label htmlFor="label" className="sr-only">
+                              Name
+                            </label>
+                            <input
+                              type="text"
+                              id="label"
+                              name="label"
+                              placeholder="Field title"
+                              value={values["title"]}
+                              onChange={handleInputChange}
+                              required
+                              className="bg-white capitalize relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            />
+                          </div>
+
+                          <div className="flex space-x-4 items-center">
+                            <label htmlFor="" className="text-black">
+                              Field Type
+                            </label>
+                            <FieldTypesDropdown values={values} setValues={setValues} />
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={addFieldToArray}
+                  >
+                    Add
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
+
+export default FieldModal;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 export { default as Navbar } from "../components/Navbar";
 export { default as Loader } from "../components/Loader";
-export { default as Dropdown } from "../components/Dropdown";
+export { default as Dropdown } from "./Dropdown";
 export { default as Modal } from "../components/Modal";
+export { default as FieldModal } from "../components/FieldModal";
+export { default as FieldTypesDropdown } from "../components/FieldTypesDropdown";
