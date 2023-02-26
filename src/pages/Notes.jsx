fec1129d41e5475195cf607dad7ef932b6b35361
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-26 20:01:35 +0530

    populating all notes

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -2,23 +2,45 @@ import React, { useState, useEffect } from "react";
 
 import client from "../utils/client";
 
+import { Card, Loader } from "../components";
+
 const Notes = () => {
   const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const fetchNotes = async () => {
       const resp = await client.fetch(`*[_type == "notes"]`);
       setNotes(resp);
     };
-
-    fetchNotes();
+    try {
+      fetchNotes();
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
   }, []);
 
   return (
     <div>
-      {notes?.map((note) => {
-        return (<p key={note?._id} className="text-black">{note?.title}</p>)
-      })}
+      <div className="absolute top-1/2 left-1/2">{loading && <Loader />}</div>
+
+      <h1 className="text-black text-center my-2 font-semibold">All notes</h1>
+
+      <section className="mt-6 grid gap-4 sm:grid-cols-2 md:grid-cols-4">
+        {notes?.reverse()?.map((note) => {
+          return (
+            <Card
+              key={note?._id}
+              title={note?.title}
+              content={note?.note}
+              slug={note?.slug}
+            />
+          );
+        })}
+      </section>
     </div>
   );
 };
