78056a4ec35e3362bb1fbb568219dd4123943380
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-26 17:35:16 +0530

    made changes to use sanity API

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,9 +1,11 @@
 import { createClient } from "@sanity/client";
 
-const client =  createClient({
-    projectId: "8mzsiel8",
-    dataset: "production",
-    useCdn: true,
-})
+const client = createClient({
+  projectId: "8mzsiel8",
+  dataset: "production",
+  token:
+    "skE3Bm2hrtKWa014xJoaNbylEZMb4XB1j0iekLXGrPlpitjnWPCt7Zq86zaIoqKCiYX5jYm6Jl3jrNOJDmMSAMwEOotjWsb6uUr2Lm60F7guh31hwfSpddauNUnDC9X508dtTS4lfxN5b8roen0cu9cb69ubTyJ3XIICNU0E9wf8H4wGHMc5",
+  useCdn: false,
+});
 
 export default client;
