439606cc9a8542fc30ba649611846d7752367c51
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-27 18:03:26 +0530

    using .env file for secret keys

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,13 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDPmRpRI7hwW4P287BFZ8SyN0iJZp8Q6WY",
-  authDomain: "medical-notes-app-ba9a5.firebaseapp.com",
-  projectId: "medical-notes-app-ba9a5",
-  storageBucket: "medical-notes-app-ba9a5.appspot.com",
-  messagingSenderId: "239544613447",
-  appId: "1:239544613447:web:ba41a8264461a043674f62"
+  apiKey: import.meta.env.VITE_SOME_KEY_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_SOME_KEY_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_SOME_KEY_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_SOME_KEY_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_SOME_KEY_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_SOME_KEY_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
--- src/utils/client.js
+++ src/utils/client.js
@@ -1,10 +1,9 @@
 import { createClient } from "@sanity/client";
 
 const client = createClient({
-  projectId: "8mzsiel8",
-  dataset: "production",
-  token:
-    "skE3Bm2hrtKWa014xJoaNbylEZMb4XB1j0iekLXGrPlpitjnWPCt7Zq86zaIoqKCiYX5jYm6Jl3jrNOJDmMSAMwEOotjWsb6uUr2Lm60F7guh31hwfSpddauNUnDC9X508dtTS4lfxN5b8roen0cu9cb69ubTyJ3XIICNU0E9wf8H4wGHMc5",
+  projectId: import.meta.env.VITE_SOME_KEY_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SOME_KEY_SANITY_DATASET,
+  token: import.meta.env.VITE_SOME_KEY_SANITY_API_TOKEN,
   useCdn: false,
 });
 
