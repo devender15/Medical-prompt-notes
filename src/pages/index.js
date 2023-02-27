f433d7720c5b8a857dea236f6272f0503202952a
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-27 14:08:14 +0530

    added 404 page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,4 @@ export { default as EditNote } from './EditNote';
 export { default as CreateNote } from './CreateNote';
 export { default as Login } from './Auth/Login';
 export { default as Register } from './Auth/Register';
+export { default as NotFound } from './NotFound';
