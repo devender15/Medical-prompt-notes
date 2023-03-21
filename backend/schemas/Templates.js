d78a839702a3fd395dde6b22b9221a677d12000d
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-03-22 00:05:48 +0530

    added the bodyText field

--- backend/schemas/Templates.js
+++ backend/schemas/Templates.js
@@ -9,31 +9,41 @@ export default {
       type: 'string',
     },
     {
-        name: 'fields',
-        title: 'Fields',
-        type: 'array',
-        of: [
-          {
-            type: 'object',
-            fields: [
-              {
-                name: 'name',
-                title: 'Name',
-                type: 'string',
-              },
-              {
-                name: 'label',
-                title: 'Label',
-                type: 'string',
-              },
-              {
-                name: 'type',
-                title: 'Field Type',
-                type: 'string',
-              },
-            ]
-          }
-        ],
+      name: 'bodyText',
+      title: 'Body text',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            {
+              name: 'name',
+              title: 'Name',
+              type: 'string',
+            },
+            {
+              name: 'label',
+              title: 'Label',
+              type: 'string',
+            },
+            {
+              name: 'type',
+              title: 'Type',
+              type: 'string',
+            },
+            {
+              name: 'text',
+              title: 'Text',
+              type: 'string',
+            },
+            {
+              name: 'isField',
+              title: 'isField',
+              type: 'boolean',
+            },
+          ]
+        },
+      ],
     },
   ],
 }
