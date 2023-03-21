1366d55e5ea638e2afa8b2711ab054bf033aa3bc
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-03-22 00:36:45 +0530

    now showing complete note perfectly

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -8,7 +8,22 @@ const Card = ({ title, content, slug }) => {
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {title}
         </h5>
-      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{content}</p>
+      {/* <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{content}</p> */}
+      <div className="my-2 space-y-2">
+                {Object.keys(content)?.map((line, idx) =>
+                  line === "note-title" ? (
+                    <p key={idx}>---- {content[line]} ----</p>
+                  ) : (
+                    <div
+                      key={idx}
+                      className="flex items-center justify-start w-1/2 mx-auto mt-4"
+                    >
+                      <p className="font-semibold mr-4 capitalize">{line === "pname" ? "Patient's name" : line} : </p>
+                      <span className="capitalize">{content[line]}</span>
+                    </div>
+                  )
+                )}
+              </div>
       <Link
         to={`/note/${slug}`}
         className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 hover:text-white rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
